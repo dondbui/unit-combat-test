8dad261edaa424c219afb6c4b54b790119c4a3f1
dondbui <dondbui@gmail.com>
2017-05-14 22:27:28 -0700

    Initial commit for the new unit-combat-test Unity project

---
+++ Assets/Scripts/CombatTest.cs
@@ -0,0 +1,23 @@
+﻿/*** ---------------------------------------------------------------------------
+/// CombatTest.cs
+/// 
+/// <author>Don Duy Bui</author>
+/// <date>May 14th, 2017</date>
+/// ------------------------------------------------------------------------***/
+
+using UnityEngine;
+
+public class CombatTest : MonoBehaviour
+{
+
+    public void Start ()
+    {
+        
+    }
+    
+    // Update is called once per frame
+    public void Update ()
+    {
+    
+    }
+}
