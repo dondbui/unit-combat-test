e39a1a23eaebb88e832daa7fd002f0b73347d28a
dondbui <dondbui@gmail.com>
2017-05-18 00:43:29 -0700

    Handle multiple shots per attack and take into account hit chance

--- Assets/Scripts/core/combat/CombatUtils.cs
+++ Assets/Scripts/core/combat/CombatUtils.cs
@@ -5,6 +5,7 @@
 /// <date>May 15th, 2017</date>
 /// ------------------------------------------------------------------------***/
 
+using core.data.vo;
 using core.units;
 using UnityEngine;
 
@@ -15,60 +16,92 @@ namespace core.combat
     /// </summary>
     public class CombatUtils
     {
+        /// <summary>
+        /// Handle the attack between 2 combatants
+        /// </summary>
         public static void HandleAttack(ref Unit attacker, int atkEqpSlot, ref Unit defender, int defEqpSlot)
         {
             Equipment attackerEquipment = attacker.equipment[atkEqpSlot];
+            ApplyDamage(ref attacker, ref attackerEquipment, ref defender);
 
-            Debug.Log("Start attack : " + attacker.vo.uid + " uses " + 
-                attackerEquipment.vo.uid +" on " + defender.vo.uid);
 
-            if (attackerEquipment.remainingAmmo == 0)
+            // Unit is dead so no counter attack.
+            if (defender.hp <= 0)
+            {
+                return;
+            }
+
+            Equipment defenderEquipment = defender.equipment[atkEqpSlot];
+            ApplyDamage(ref defender, ref defenderEquipment, ref attacker);
+        }
+
+        /// <summary>
+        /// Applies damage to the defender
+        /// </summary>
+        private static void ApplyDamage(ref Unit attacker, ref Equipment equipment, ref Unit defender)
+        {
+            EquipmentVO equipmentVO = equipment.vo;
+
+            Debug.Log(attacker.vo.uid + " uses " +
+                equipmentVO.uid + " on " + defender.vo.uid);
+
+            if (equipment.remainingAmmo == 0)
             {
                 Debug.Log(attacker.vo.uid + " NO AMMO!!");
                 return;
             }
 
             // calculate the damage
-            int damage = attackerEquipment.vo.basedamage;
+            int baseShotDamage = equipmentVO.basedamage;
+            int numShots = equipmentVO.numshotsperattack;
+            double hitChance = equipmentVO.hitchance;
 
-            // apply the damage
-            defender.hp -= damage;
-
-            // prevent it from going negative.
-            if (defender.hp <= 0)
+            // do the num shots
+            for (int i = 0; i < numShots; i++)
             {
-                defender.hp = 0;
+                // TODO: apply dodge reduction
+                if (!DoesShotHit(hitChance, 0))
+                {
+                    Debug.Log("MISS!!!");
+                    continue;
+                }
 
-                Debug.Log(defender.vo.uid + " DESTROYED!!");
+                // apply the damage
+                defender.hp -= baseShotDamage;
+
+                Debug.Log("DAMAGE: " + baseShotDamage);
+
+                // prevent it from going negative.
+                if (defender.hp <= 0)
+                {
+                    defender.hp = 0;
+
+                    Debug.Log(defender.vo.uid + " DESTROYED!!");
+                    break;
+                }
+
+                // expend some ammo
+                int ammoUsed = 1;
+                equipment.remainingAmmo -= ammoUsed;
+                if (equipment.remainingAmmo <= 0)
+                {
+                    Debug.Log(equipmentVO.uid + " Out of AMMO!!");
+                    break;
+                }
             }
-
-            // expend some ammo
-            int ammoUsed = 1;
-            attackerEquipment.remainingAmmo -= ammoUsed;
-
-            Equipment defenderEquipment = defender.equipment[defEqpSlot];
-
-            if (defenderEquipment.remainingAmmo == 0)
-            {
-                Debug.Log(defender.vo.uid + " NO AMMO!!");
-                return;
-            }
-
-            // now it's the defender's turn to fire back
-            Debug.Log("Counter-Attack : " + defender.vo.uid + " uses " +
-                defenderEquipment.vo.uid + " on " + attacker.vo.uid);
-
-            int counterDamage = defenderEquipment.vo.basedamage;
-
-            attacker.hp -= counterDamage;
-            // prevent it from going negative.
-            if (attacker.hp <= 0)
-            {
-                attacker.hp = 0;
-
-                Debug.Log(attacker.vo.uid + " DESTROYED!!");
-            }
-            defenderEquipment.remainingAmmo -= ammoUsed;
         }
+
+        /// <summary>
+        /// Does the shot hit given the attacker's hit chance and the defender's dodge chance
+        /// </summary>
+        private static bool DoesShotHit(double hitChance, double dodgeChange)
+        {
+            // Roll for chance to hit
+            float roll = Random.Range(0, 1.0f);
+
+            // If we roll under the threshold then we're good
+            return roll <= hitChance - dodgeChange;
+        }
+
     }
 }
